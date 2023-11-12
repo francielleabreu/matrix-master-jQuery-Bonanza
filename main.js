ec25e7ceb887a84aeb40f93cf257daa01a51532b
Francielle Abreu da Silva <fran.abreu2@gmail.com>
2023-11-12 16:11:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Fade out effect
+$("#btn-fade-out").click(function () {
+    $("#textFadeOut").fadeOut();
+});
+
+// Append text
+$("#btn-append").click(function () {
+    $("#textAppend").append(" Welcome to jQuery");
+});
+
+// Change color to red
+$("#btn-change-color").click(function () {
+    $("#changeColorBtn").css("color", "red");
+});
